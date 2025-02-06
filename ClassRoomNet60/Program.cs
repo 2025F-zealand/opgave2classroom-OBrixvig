490dab1e0a48f3da490b91ef4602c587027aeba5
Oliver Colding Brixvig <olb005@edu.zealand.dk>
2025-02-06 13:19:49 +0100

    Har oprettet et classroom, og nogle students som er tildetls classrooms. og giver classroom et navn og en startdato

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ClassRoomNet60;
+
 Console.WriteLine("Hello, World!");
+
+ClassRoom classRoom1 = new ClassRoom();
+
+classRoom1.ClassName = "3Q";
+classRoom1.SemesterStart = new DateTime (2019,08,26);
+
+Student oliver = new Student("Oliver", 12, 04);
+Student kevin = new Student("Kevin", 12, 02); 
+
+classRoom1.StudentList.Add(oliver);
+classRoom1.StudentList.Add(kevin);
+
+
