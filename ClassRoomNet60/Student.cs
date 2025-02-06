1646a33b1f478905ef8ff6f21a1d9d9bd927592e
Oliver Colding Brixvig <olb005@edu.zealand.dk>
2025-02-06 13:09:47 +0100

    Jeg har oprettet en public class som hedder Stundent. Derefter har jeg oprettet public properties Lavet en constorctor der tage alle properties med som parameter

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+  public class Student
+    {
+
+        public string Name {  get; set; }
+        public int Birthday { get; set; }
+        public int BirthMonth {  get; set; }
+
+        public Student(string name, int birthday, int birthmonth)
+        {
+            Name = name;
+            Birthday = birthday;
+            BirthMonth = birthmonth;
+        }
+    }
+}
